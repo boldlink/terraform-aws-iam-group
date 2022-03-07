5d642fa9d4b3fa5f0b3eac2476a3b468f6dfdc22
patrick <mukumbu.katiwa@boldlink.io>
2022-03-07 13:21:32 +0300

    NOSTORY: First IAM Group module commit

---
+++ outputs.tf
@@ -0,0 +1,44 @@
+output "iam_group_id" {
+  value       = aws_iam_group.main.id
+  description = "The group's ID."
+}
+
+output "iam_group_arn" {
+  value       = aws_iam_group.main.arn
+  description = "The ARN assigned by AWS for this group."
+}
+
+output "iam_group_name" {
+  value       = aws_iam_group.main.name
+  description = "The group's name."
+}
+
+output "iam_group_path" {
+  value       = aws_iam_group.main.path
+  description = "The path of the group in IAM."
+}
+
+output "iam_group_unique_id" {
+  value       = aws_iam_group.main.unique_id
+  description = "The unique ID assigned by AWS."
+}
+
+output "iam_group_policy_name" {
+  value       = aws_iam_group_policy.main.name
+  description = "The name of the policy."
+}
+
+output "iam_group_policy" {
+  value       = aws_iam_group_policy.main.policy
+  description = "The policy document attached to the group."
+}
+
+output "group_membership_name" {
+  value       = aws_iam_group_membership.main.name
+  description = "The name to identify the Group Membership"
+}
+
+output "group_membership_users" {
+  value       = aws_iam_group_membership.main.users
+  description = "list of IAM User names"
+}
