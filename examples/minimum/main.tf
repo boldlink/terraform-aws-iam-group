0851c48f7d15f41e87b62215e7ab1fe62282c857
patrick <mukumbu.katiwa@boldlink.io>
2022-07-06 12:13:46 +0300

    NOSTORY: added checkov skip to root

--- examples/minimum/main.tf
+++ examples/minimum/main.tf
@@ -1,6 +1,4 @@
 module "minimum_group" {
-  #checkov:skip=CKV2_AWS_21:Ensure that all IAM users are members of at least one IAM group
-  #checkov:skip=CKV2_AWS_14:Ensure that IAM groups includes at least one IAM user
   source     = "../../"
   group_name = "boldlink-minimum-example-group"
 }
